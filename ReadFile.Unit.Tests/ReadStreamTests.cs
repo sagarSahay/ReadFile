2f0cd716cc8690af6497dddf6e7c5a6a44a3fd4a
sagar_ph <sagar@parcelvision.com>
2018-10-05 14:14:40 +0100

    initial commit

---
+++ ReadFile.Unit.Tests/ReadStreamTests.cs
@@ -0,0 +1,32 @@
+﻿namespace ReadFile.Unit.Tests
+{
+    using System.IO;
+    using FluentAssertions;
+    using Microsoft.Extensions.Configuration;
+    using Xunit;
+
+    public class ReadStreamTests
+    {
+
+        [Fact]
+        public void Read_WithProperPath_ShouldReadFile()
+        {
+            // Arrange
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            var configuration = builder.Build();
+
+            var filePath = configuration.GetSection("FilePath").Value;
+            var SUT = new ReadStream("abcd");
+
+            // Act 
+
+            var result = SUT.ReadLine();
+
+            // Assert
+
+            result.Should().NotBe(string.Empty);
+        }
+    }
+}
---
+++ ReadFile/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ReadFile
+{
+    using System.IO;
+    using Microsoft.Extensions.Configuration;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            var configuration = builder.Build();
+
+            var filePath = configuration.GetSection("FilePath").Value;
+
+            while (Read(filePath, out var result))
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        public static bool Read(string filePath, out string val)
+        {
+            var readStream = new ReadStream(filePath);
+            val = readStream.ReadLine();
+            return string.IsNullOrEmpty(val);
+        }
+    }
+
+    public interface IReadStream : IDisposable
+    {
+        string ReadLine();
+    }
+
+    public class ReadStream : IReadStream
+    {
+        private string fileName;
+        private Lazy<StreamReader> streamReaderLazy;
+
+        public ReadStream(string fileName)
+        {
+            this.fileName = fileName;
+            this.streamReaderLazy = new Lazy<StreamReader>(() => new StreamReader(fileName));
+        }
+        public void Dispose()
+        {
+            streamReaderLazy?.Value?.Dispose();
+        }
+
+        public string ReadLine()
+        {
+            return streamReaderLazy.Value.ReadLine();
+        }
+    }
+}
